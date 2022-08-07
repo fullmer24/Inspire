84c3610cda92feb0d0d759c171a7e70f2983b4dd
kade fullmer <fullmer24@gmail.com>
2022-08-07 15:54:02 -0600

    weather updated

--- app/Controllers/WeatherController.js
+++ app/Controllers/WeatherController.js
@@ -5,7 +5,7 @@ import { Pop } from "../Utils/Pop.js";
 
 function _drawWeather() {
     // @ts-ignore
-    document.getElementById('weather').innerHTML = ((`${ProxyState.weather}` - 273.15) * 1.8) + 32;
+    document.getElementById('weather').innerHTML = Math.round(((`${ProxyState.weather}` - 273.15) * 1.8) + 32);
 }
 
 
@@ -29,7 +29,7 @@ export class WeatherController {
 
     fToC() {
         // @ts-ignore
-        document.getElementById('weather').innerHTML = (`${ProxyState.weather}` - 273.15);
+        document.getElementById('weather').innerHTML = Math.round((`${ProxyState.weather}` - 273.15));
     }
 }
 
--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -1,6 +1,7 @@
 export class Weather {
     constructor(data) {
-        this.temp = data.main.temp
+        // this.temp = data.main.temp
+        this.temp = Math.round(data.main.temp)
     }
 
     get WeatherTemplate() {
