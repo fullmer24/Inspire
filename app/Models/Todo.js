5d84a812a059f70ab5af67144cd3489abde75317
kade fullmer <fullmer24@gmail.com>
2022-08-07 13:01:58 -0600

    ToDos and form drawn to page

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -3,6 +3,12 @@ import { generateId } from "../Utils/generateId.js";
 export class Todo {
     constructor(data) {
         this.id = data.id || generateId
+        this.todo = data.todo
+    }
 
+    get Template() {
+        return `
+        
+        `
     }
 }
