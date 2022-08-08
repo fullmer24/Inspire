1c9e49d8a947ebf834eaf0c632fe09f23f4f8e2f
kade fullmer <fullmer24@gmail.com>
2022-08-07 21:28:50 -0600

    todo written with errors

--- app/Controllers/TodosController.js
+++ app/Controllers/TodosController.js
@@ -8,15 +8,31 @@ import { Pop } from "../Utils/Pop.js"
 
 function _drawTodos() {
     let template = ''
+    // @ts-ignore
     ProxyState.todos.forEach(t => template += t.TodosTemplate)
     console.log(ProxyState.todos);
-    document.getElementById('totalTodos').innerHTML = template
+    // @ts-ignore
+    document.getElementById('totalTodos').innerText = template
+    // @ts-ignore
+    document.getElementById('toggledTodos').innerHTML = `
+<div>
+    <div>
+        <div>
+        <b>Total Todos:</b> ${ProxyState.todos.length}
+        </div>
+        <div>
+        <b>Finished Todos:</b> ${ProxyState.todos.filter(t => t.checked).length}
+        </div>
+    </div>
+</div>
+`
 
 
 }
 
 export class TodosController {
     constructor() {
+        console.log(`todos controller`)
         ProxyState.on('todos', _drawTodos)
         this.getTodos()
     }
@@ -24,6 +40,7 @@ export class TodosController {
 
     async getTodos() {
         try {
+            console.log(`getting todos`);
             await todosService.getTodos()
         } catch (error) {
             console.error('[Getting todos]', error)
@@ -31,7 +48,38 @@ export class TodosController {
         }
     }
 
+    async addTodo() {
+        try {
+            console.log(`adding todos`);
+            await todosService.addTodo()
+        } catch (error) {
+            console.error('[Adding Todo]', error)
+            Pop.error(error)
+        }
+    }
 
+    async deleteTodo(id) {
+        try {
+            console.log(`deleting todos`);
+            const yes = await Pop.confirm('Delete Todo')
+            if (!yes) { return }
+
+            await todosService.deleteTodo(id)
+        } catch (error) {
+            console.error('[Delete Todo]', error)
+            Pop.error(error)
+        }
+    }
+
+    async toggleTodo(id) {
+        try {
+            console.log(`finishing todos`);
+            await todosService.toggleTodo(id)
+        } catch (error) {
+            console.error('[Toggle Todo]', error)
+            Pop.error(error)
+        }
+    }
 
 
 
--- app/Services/TodosService.js
+++ app/Services/TodosService.js
@@ -4,22 +4,42 @@ import { sandboxApi } from "./AxiosService.js"
 
 class TodosService {
     async toggleTodo(id) {
-
+        // @ts-ignore
+        let todo = ProxyState.todos.find(t => t.id == id)
+        if (!todo) {
+            throw new Error('Bad Todo id')
+        }
+        todo.checked = !todo.checked
+        // @ts-ignore
+        let todoIndex = ProxyState.todos.indexOf(todo)
+        let res = await sandboxApi.put(`/Kade/todos/${id}`, todo)
+        let updatedTodo = new todo(res.data)
+        // @ts-ignore
+        ProxyState.todos.splice(todoIndex, 1, updatedTodo)
+        ProxyState.todos = ProxyState.todos
     }
 
     async deleteTodo(id) {
-
+        await sandboxApi.delete(`/Kade/todos/${id}`)
+        ProxyState.todos = ProxyState.todos.filter(t => t.id != id)
     }
-
-
 
     async getTodos() {
         let res = await sandboxApi.get('/Kade/todos')
         ProxyState.todos = res.data.map(t => new Todo(t))
-        // console.log(ProxyState.todos);
     }
 
     async addTodo() {
+        if (ProxyState.todos.find(t => t.todo == ProxyState.todos)) {
+            throw new Error('You already have this todo')
+        }
+
+        let res = await sandboxApi.post('/Kade/todos', ProxyState.todos)
+
+        let newTodo = new Todo(res.data)
+        ProxyState.todos = [...ProxyState.todos, newTodo]
+
+
 
     }
 }
